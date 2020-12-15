102458b3ae5b908299dfd922eb139c4ef6f1727b
lbrink <lars.brinkschmidt@gmail.com>
2020-12-15 12:05:25 +0100

    finished day 14

--- 2020_14.py
+++ 2020_14.py
@@ -1,5 +1,10 @@
 from lbrinks_helpers import load_to_list
 from itertools import product
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format='%(levelname)s:%(message)s')
+logging.disable(logging.CRITICAL)
+
 
 def decoder_v_1(instructions):
     mem = {}
@@ -15,23 +20,30 @@ def decoder_v_1(instructions):
 
 def bitmask(value, mask, unchanged ="X"):
     bits = list(bin(int(value))[2:].zfill(36))
+    test = "".join(bits)
     new_value = [bits[i] if mask[i]==unchanged else mask[i] for i in range(36)]
     return int("".join(new_value), 2)
 
-def floating_bits(mask):
-    masks = []
+def floating_bits(address, mask):
+    adresses = []
     x_count = mask.count("X")
     binaries = [list(bin(x)[2:].zfill(x_count)) for x in range(2**x_count)]
+    logging.debug(f"mask: {mask}")
     for binary in binaries:
+        masked = list(bin(int(address))[2:].zfill(36))
         x = 0
-        floating_mask = list(mask)
         for i in range(36):
-            if floating_mask[i] == "X":
-                floating_mask[i] = binary[x]
+            if mask[i] == "1":
+                masked[i] = "1"
+            if mask[i] == "X":
+                masked[i] = binary[x]
                 x += 1
-        masks.append(floating_mask)
-    ["".join(value) for value in masks]
-    return masks
+        masked = "".join(masked)
+        logging.debug(f"{masked}")
+        logging.debug(f"{int(masked,2)}")
+        adresses.append(masked)
+    logging.debug(f"masks: {adresses}")
+    return adresses
 
 def decoder_v_2(instructions):
     mem = {}
@@ -39,21 +51,20 @@ def decoder_v_2(instructions):
         parts = line.split()
         if parts[0].startswith("mem"):
             address = parts[0][4: -1]
-            addresses = [bitmask(address, mask, "0") for mask in masks]
+            addresses = floating_bits(address, mask)
+            logging.info(f"{addresses}")
             for a in addresses:
                 mem[a] = parts[2]
         else:    
-            masks = floating_bits(parts[2])
+            mask = parts[2]
     
+    print("Solution")
     print(sum([int(mem[address]) for address in mem]))
     return mem
 
 instructions = load_to_list(".\\data\\2020_14.txt")
 
+
 decoder_v_1(instructions)
 
-mem = decoder_v_2(instructions)
-s = 0
-for key in mem:
-    s += int(mem[key])
-print(s)
+decoder_v_2(instructions)
